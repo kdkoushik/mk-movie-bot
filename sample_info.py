e7f4c385778d565374bb6b529a561a667b9814b1
KD KOUSHIK <87390853+kdkoushik@users.noreply.github.com>
2024-04-17 00:05:46 +0600

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 21352815
+API_HASH = '7a0789ace80a08cdeaf1c496b4f884d9'
+BOT_TOKEN = '6884391979:AAFmYaafWAgn5XsFfGB3Hh98s7pB89xPpa8'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,14 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [6927862127,'']
+CHANNELS = [-1002045684382, -1002045684382, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'GreyMatter'
+DATABASE_URI = "mongodb+srv://koushik:iam@koushik@cluster0.ermeveq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = 'Cluster0'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 
